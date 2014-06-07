c9f313e7bff5c80998ea0449e32bc86dcfdca1e8
intlabs <petebirley@gmail.com>
2014-06-07 13:01:54 +0100

    ...

--- CannyOS/startup.sh
+++ CannyOS/startup.sh
@@ -6,8 +6,31 @@ if [ /CannyOS/post-install.sh ]; then
 	/CannyOS/post-install.sh &
 fi
 
-# Startup ssh
+echo "***************************************************************"
+echo "***************************************************************"
+echo "**                                                           **"
+echo "**                                                           **"
+echo "**      Welcome to CannyOS:  startup script starting         **"
+echo "**                                                           **"
+echo "**                                                           **"
+echo "***************************************************************"
+echo "***************************************************************"
+
+#Startup script begins:
+
+# Startup ssh server to allow incomming connections.
 /usr/sbin/sshd -D &
+
+
+echo "***************************************************************"
+echo "***************************************************************"
+echo "**                                                           **"
+echo "**                                                           **"
+echo "**      Welcome to CannyOS:  startup script finished         **"
+echo "**                                                           **"
+echo "**                                                           **"
+echo "***************************************************************"
+echo "***************************************************************"
 
 # Drop into shell
 bash
