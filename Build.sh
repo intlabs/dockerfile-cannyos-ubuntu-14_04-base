02e2bb986dbe60e133f65b93026bd80f6d06c539
intlabs <petebirley@gmail.com>
2014-06-07 15:01:22 +0100

    ...

--- Build.sh
+++ Build.sh
@@ -18,9 +18,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-
-cat /CannyOS/CannyOS.splash
+cat https://raw.githubusercontent.com/intlabs/dockerfile-cannyos-ubuntu-14_04-base/master/CannyOS/CannyOS.splash
 #     *****************************************************
 #     *                                                   *
 #     *        _____                    ____  ____        *
